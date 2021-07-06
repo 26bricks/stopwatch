a206f862d1ead0ceec312a0e6bf567d8ec376463
26bricks <26.bricks@gmail.com>
2021-07-06 19:49:25 +0200

    Pridani AppWrapper.js kvuli testovani

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AppWrapper from './components/AppWrapper'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <AppWrapper />
+  );
+
+  const linkElement = screen.getByText(/start/i);
   expect(linkElement).toBeInTheDocument();
 });
